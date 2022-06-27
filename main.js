d331d64cdb06b0cf22aac1ecbf39f899e750f612
Dawitlior <dawitlior777@gmail.com>
2022-06-27 16:24:53 +0300

    add

---
+++ main.js
@@ -0,0 +1,24 @@
+const API_KEY = "123cedf472ea7d740a81046892916adb";
+const WEATHER_BASIC_API = "https://api.openweathermap.org/data/2.5/";
+const theDiv = document.getElementById("my_div")
+
+async function getTheWeather() {
+  try {
+    return await fetch(
+      `${WEATHER_BASIC_API}weather?q=${cityName}&appid=${API_KEY}`
+    ).then((res) => res.json());
+  } catch (err) {
+    alert(err);
+  } finally {
+  }
+}
+
+let cityName = cityNameSelect.value;
+function printWeather() {
+  getTheWeather(cityName).then(result => {
+    for (let item in result) {
+      theDiv.innerHTML += `<p>${item}${result[key]}</p>`;
+    }
+  });
+}
+
